d235f5dcd4f8ad37621318be36baa39a0f19c22b
sudarshansivakumar <63507238+sudarshansivakumar@users.noreply.github.com>
2021-09-15 11:52:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,18 +17,13 @@ model_checkpoint = "distilbert-base-cased"
 
 tokenizer = AutoTokenizer.from_pretrained(model_checkpoint)
 
+# The model_path here would be the directory in which you saved the model using the HuggingFace model.save_pretrained() function
 model_path = "C:/Users/Sudarshan_backup/Desktop/Datasets/QA_Model/PreTrained_epoch2"
 myQAModel = DistilBertForQuestionAnswering.from_pretrained(model_path)
 
 QAPipeline = QuestionAnsweringPipeline(model = myQAModel,tokenizer = tokenizer)
 
-#example_context = "From childhood through most of his professional career, Nadal was coached by his uncle Toni. He was one of the most successful teenagers in ATP Tour history, reaching No. 2 in the world at age 19 and winning 16 titles, including his first French Open and six Masters events. Nadal became No. 1 for the first time in 2008 after his first major victory off clay against the longtime top-ranked Federer, his main rival through 2010, in a historic Wimbledon final. He also won an Olympic gold medal in singles that year in Beijing. After defeating Djokovic in the 2010 US Open final, the 24-year-old Nadal became the youngest male tennis player in the Open Era to achieve the career Grand Slam, and also became the first male tennis player to win three Grand Slams on three different surfaces (hard, grass and clay) the same calendar year. With his Olympic gold medal, he is also one of only two male players to complete the career Golden Slam"
-#example_question = "Where did he win an Olympics prize?"
-
-#pipe_outputs = QAPipeline(question = example_question,context = example_context,topk = 3, max_seq_len = 512)
-
-#print(pipe_outputs)
-#@st.cache
+# This is a markdown message at the beginning of my application in which I'm introducing myself and explaining the question. You should add whatever message you want to. 
 st.markdown("Hello, I am Sudarshan Sivakumar, a 3rd year Undergraduate in Computer Science and Engineering at the Manipal Institute of Technology. This is a Question Answering System I made using the Hugging Face transformers library on the Stanford Question Answering dataset. The model has been deployed using Streamlit")
 
 st.markdown("If you enter a context passage and ask a question whose answer is in the context, the program will give you an answer span from the passage which best answers the question")
